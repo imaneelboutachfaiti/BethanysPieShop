1dcd65262a5ffb5dae2dc6a3bf7ba52e50a405cc
Imane El Boutachfaiti <imane.elboutachfaiti@gmail.com>
2020-04-04 12:45:13 +0100

    Adding validation

--- BethanysPieShop/Models/Order.cs
+++ BethanysPieShop/Models/Order.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,17 +11,62 @@ namespace BethanysPieShop.Models
     {
         public int OrderId { get; set; }
         public List<OrderDetail> OrderDetails { get; set; }
+
+        [Required(ErrorMessage ="Please enter your first name")]
+        [Display(Name = "First name")]
+        [StringLength(50)]
         public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Please enter your first name")]
+        [Display(Name = "Last name")]
+        [StringLength(50)]
         public string LastName { get; set; }
+
+        [Required(ErrorMessage ="Please enter your adress")]
+        [StringLength(100)]
+        [Display(Name = "Adress line 1")]
         public string AdresseLine1 { get; set; }
+
+        [Display(Name = "Adress line 1")]
         public string AdresseLine2 { get; set; }
+
+        [Required(ErrorMessage = "Please enter your zip code")]
+        [Display(Name = "Zip code")]
+        [StringLength(10, MinimumLength =4)]
         public string ZipCode { get; set; }
+
+        [Required(ErrorMessage = "Please enter your city")]
+        [StringLength(50)]
         public string City { get; set; }
+
+        [StringLength(10)]
         public string State { get; set; }
+
+        [Required(ErrorMessage = "Please enter your country")]
+        [StringLength(50)]
         public string Country { get; set; }
+
+        [Required(ErrorMessage = "Please enter your phone number")]
+        [DataType(DataType.PhoneNumber)]
+        [StringLength(25)]
+        [Display(Name ="Phone number")]
         public string PhoneNumber { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        [DataType(DataType.EmailAddress)]
+        [RegularExpression(@"^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}" +
+         @"\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\" +
+         @".)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$",
+         ErrorMessage ="The email adress is not entered in a correct format")]
         public string Email { get; set; }
+
+        [BindNever]
+        [ScaffoldColumn(false)]
         public decimal OrderTotal { get; set; }
+
+        [BindNever]
+        [ScaffoldColumn(false)]
         public DateTime OrderPlaced { get; set; }
     }
 }
